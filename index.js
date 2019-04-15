1e27ceedb0b3fcb742e2d67fb71c2dea4469774b
Raghunath R <raghunath.r@insdc01.pwc.com>
2019-04-15 18:42:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+let app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
