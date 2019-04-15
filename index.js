8d7cfa87c78025d53659262d182c978c0087d453
Raghunath R <raghunath.r@insdc01.pwc.com>
2019-04-15 18:48:59 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.use(express.static(__dirname + '/public/'));
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
     console.log("Server is running on port " + port);
+    console.log("App is Running!!!");
+    
 });
